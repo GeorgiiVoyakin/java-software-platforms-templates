0a28e6745b1649e0d7348cce754432e89e8a1297
GeorgiiVoyakin <voyaking@gmail.com>
2021-04-10 02:58:46 +0300

    task 9 finished

---
+++ task09/src/main/java/ru/mirea/Main.java
@@ -0,0 +1,15 @@
+package ru.mirea;
+
+/**
+ * @author Георгий Воякин
+ * @version 1.0
+ */
+public class Main {
+    /**
+     * Метод печатает сообщение в консоль
+     * @param args аргументы командной строки (не используются)
+     */
+    public static void main(String[] args) {
+        System.out.println("This app is built with Gradle");
+    }
+}
