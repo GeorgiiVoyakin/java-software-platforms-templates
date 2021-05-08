5a30135e4cecaf4d4c5877ac9101fd2e1599dac5
GeorgiiVoyakin <voyaking@gmail.com>
2021-05-08 23:05:38 +0300

    task 11 finished

---
+++ task11/src/main/java/ru/mirea/ikbo1319/task11/Task11Application.java
@@ -0,0 +1,13 @@
+package ru.mirea.ikbo1319.task11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Task11Application.class, args);
+    }
+
+}
