959f3728a950005ed05eee28d5b208a0db24c504
GeorgiiVoyakin <voyaking@gmail.com>
2021-05-29 01:09:51 +0300

    task 12 finished

---
+++ task12/src/main/java/ru/mirea/ikbo1319/task12/Task12Application.java
@@ -0,0 +1,18 @@
+package ru.mirea.ikbo1319.task12;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task12Application implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Task12Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+
+    }
+}
---
+++ task12/src/test/java/ru/mirea/ikbo1319/task12/Task12ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mirea.ikbo1319.task12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
