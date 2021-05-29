8050895e9c7cdab0f399563d07fdbe8ef6f09f8d
GeorgiiVoyakin <voyaking@gmail.com>
2021-05-29 03:05:45 +0300

    task 13 finished

---
+++ task13/src/main/java/ru/mirea/ikbo1319/task13/Task13Application.java
@@ -0,0 +1,13 @@
+package ru.mirea.ikbo1319.task13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task13Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Task13Application.class, args);
+    }
+
+}
---
+++ task13/src/test/java/ru/mirea/ikbo1319/task13/Task13ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mirea.ikbo1319.task13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task13ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
